e7a36fb5f2ea0dc7ae01ea01a24b4656060e9109
Pasindu Weerasinghe <pasindurohana@gmail.com>
2018-01-17 22:09:21 +0530

    add virtutueMart and payment methods

---
+++ administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,6 @@
+#
+#<?php die("Forbidden."); ?>
+
+2018-01-17 14:08:22 ERROR vmError: Warning, the Safe Path is not configured yet. Use this link to the &lt;a href='http://localhost/joomla3x/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
+2018-01-17 14:16:21 ERROR vmError: Warning, the Safe Path is not configured yet. Use this link to the &lt;a href='http://localhost/joomla3x/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
+2018-01-17 14:16:54 ERROR vmError: Warning, the Safe Path is not configured yet. Use this link to the &lt;a href='http://localhost/joomla3x/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
